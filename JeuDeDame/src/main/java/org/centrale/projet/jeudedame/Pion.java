f0a93794bf0cf072e079f926e40a480bda828cfb
Antoine GACHET3 <agachet44@gmail.com>
2025-12-08 15:07:54 +0100

    Merge origin/main into main
    # Conflicts:
    #	Monopoly/src/main/java/org/centrale/medev/monopoly/monopoly/Joueur.java

--- JeuDeDame/src/main/java/org/centrale/projet/jeudedame/Pion.java
+++ JeuDeDame/src/main/java/org/centrale/projet/jeudedame/Pion.java
@@ -10,4 +10,24 @@ package org.centrale.projet.jeudedame;
  */
 public class Pion {
     
+    
+    public void deplace(int a){
+        if (this.couleur=="blanc"){
+            if (a==1){
+                this.position.sety=this.position.gety-1;
+                this.position.setx=this.position.getx+1;    
+            }else{
+                this.position.sety=this.position.gety+1;
+                this.position.setx=this.position.getx+1;
+            }
+        }else{
+            if (a==1){
+                this.position.sety=this.position.gety-1;
+                this.position.setx=this.position.getx-1;    
+            }else{
+                this.position.sety=this.position.gety+1;
+                this.position.setx=this.position.getx-1;
+            }
+        }
+    }
 }
